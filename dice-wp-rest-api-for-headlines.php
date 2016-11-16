6c4a996862da73d6917d76e3bc00418da8aa92ab
Joshua Smith <joshsmith01@me.com>
2016-11-16 09:52:34 -0800

    Initial commit

---
+++ dice-wp-rest-api-for-headlines.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Dice WP REST API For Headlines
+Plugin URI: https://confluence.dice.com/display/WP/Dice+WP+REST+API+For+Headlines
+Description: A brief description of the Plugin.
+Version: 2016-11-16
+Author: Josh Smith
+Author URI: http://dice.com
+License: Standard DHI Group, Inc. licensing applies
+*/
