d4784acc45594c35c11162c64f0e3594a6b62326
Joshua Smith <joshsmith01@me.com>
2016-11-30 13:06:43 -0800

    Properly enqueue the correct JavaScript on the correct admin pages

--- dice-wp-rest-api-for-headlines.php
+++ dice-wp-rest-api-for-headlines.php
@@ -30,32 +30,34 @@ require_once plugin_dir_path( __FILE__ ) . 'api/api-headlines.php';
 
 // Enqueue some scripts and styles here. -JMS
 function dwrafh_enqueue_scripts($hook) {
-	// TODO: Only load this on the admin pages where it's needed. -JMS
-//	wp_die( $hook );
-    // Use this to determine your $hook: wp_die($hook);
-    // Hook == toplevel_page_edit-headlines
-//    if ($hook != 'index.php' ) {
-//        return;
-//    } else {
+    // Use this to determine your $hook:
+	//wp_die($hook);
 	    wp_enqueue_style( 'dwrafh-admin-css', plugins_url( 'admin/css/admin-headlines.css', __FILE__ ) );
-	    wp_enqueue_script( 'dwrafh-admin-js', plugins_url( 'admin/js/admin-headlines.js', __FILE__ ), array(
-		    'jquery',
-		    'media-upload',
-		    'jquery-ui-tabs'
-	    ), '20161115', true );
 
-	    wp_enqueue_script( 'reorder-js', plugins_url( 'admin/js/reorder.js', __FILE__ ), array(
-		    'jquery',
-		    'jquery-ui-sortable'
-	    ), '20161115', true );
+	    // Load up the tabs and media upload where it's needed, on the post page. -JMS
+	    if ($hook === 'post.php') {
+		    wp_enqueue_script( 'dwrafh-admin-js', plugins_url( 'admin/js/admin-headlines.js', __FILE__ ), array(
+			    'jquery',
+			    'media-upload',
+			    'jquery-ui-tabs'
+		    ), '20161115', true );
+	    }
+
+	    // Load up the reorder script where it is needed, on the Dashboard page. -JMS
+		if ( $hook === 'index.php' ) {
+			wp_enqueue_script( 'reorder-js', plugins_url( 'admin/js/reorder.js', __FILE__ ), array(
+				'jquery',
+				'jquery-ui-sortable'
+			), '20161115', true );
+		}
+
+		// Move the field values from JavaScript to PHP. -JMS
 	    wp_localize_script( 'reorder-js', 'WP_HEADLINE_LISTING', array(
 		    'security' => wp_create_nonce( 'wp-headline-order' ),
 		    'success'  => 'Headlines sort order has been saved',
 		    'failure'  => 'There was an error saving the sort order, or you do not have the proper permissions.'
 	    ) );
-//	    wp_enqueue_style( 'jquery-style', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.2/themes/smoothness/jquery-ui.css' );
 
-//	wp_enqueue_script( 'admin-headlines', plugins_url( '/admin/js/admin-headlines.js', __FILE__ ), array( 'jquery' ), true );
 	wp_enqueue_script( 'flatpickr', plugins_url( '/admin/js/flatpickr.min.js', __FILE__ ), array( 'jquery' ), true );
 	wp_localize_script( 'dwrafh-admin-js',
 		'customUploads',
@@ -64,14 +66,7 @@ function dwrafh_enqueue_scripts($hook) {
 			'largeBannerData' => get_post_meta( get_the_ID(), 'banner_large_data', true )
 		) );
 
-//	wp_enqueue_style( 'metabox-css', plugins_url( '/admin/css/metabox.css', __FILE__ ) );
-//	wp_enqueue_style( 'flatpickr-css', plugins_url( '/admin/css/flatpickr.min.css', __FILE__ ) );
 	add_meta_box( 'headlines-metabox', __( 'Headlines Meta' ), 'dwrafh_headlines_metabox', 'post', 'normal', 'high'/*,array()*/ );
-
-
-
-
-//    }
 }
 add_action('admin_enqueue_scripts', 'dwrafh_enqueue_scripts');
 
